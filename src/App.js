58668d795cc6085c9a00c08214b28bbef67436d6
Stephen Gault <stephengault626@googlemail.com>
2021-12-20 19:05:28 +0000

    Form complete

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import '@aws-amplify/ui-react/styles.css';
 import { listNotes } from './graphql/queries';
 import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations'
 import { API, Storage } from 'aws-amplify';
-import { Transition } from "@headlessui/react";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlusSquare } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+library.add(faPlusSquare)
 // Started off with a notes app from AWS tutorial and amended to required function
 // Has authentication and option to login can be added using social media / google or amazon accounts
 
@@ -21,8 +24,7 @@ const initialFormState = {
  }
 
 export default function App() {
-
-  const [isOpen, setIsOpen] = useState(false);
+  
   const [notes, setNotes] = useState([]);
   const [formData, setFormData] = useState(initialFormState);
 
@@ -70,6 +72,7 @@ export default function App() {
     await Storage.put(file.name, file);
     fetchNotes();
   }
+
 
 
 
@@ -132,82 +135,200 @@ export default function App() {
       </p>
       </div>
 
+      <div className="bg-black">
+        <div className="container mx-auto">        
+          <div className="bg-black grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 md:content-start md:justify-start">
+            {
+            notes.map(note => (
+              <div key={note.id || note.name} className="w-screen md:w-64 rounded mx-auto py-5">
 
-{/* TODO: move into form on modal */}
-      <input 
-        type="text"
-        onChange={e => setFormData({ ...formData, 'name': e.target.value })}
-        placeholder='Title Name'
-        value={formData.name}
-      />
-
-      <input 
-        type="text"
-        onChange={e => setFormData({...formData, 'description': e.target.value})}
-        placeholder='System Platform'
-        value={formData.description}
-      />
-
-      <input 
-        type="text"
-        onChange={e => setFormData({...formData, 'genre': e.target.value})}
-        placeholder='Title Genre'
-        value={formData.genre}
-      />
-
-      <input
-        type="date" 
-        onChange={e => setFormData({...formData, 'releaseDate': e.target.value})}
-        placeholder='Release Date'
-        value={formData.releaseDate}
-      />
-
-      <input
-        type="number"
-        onChange={e => setFormData({...formData, 'players': e.target.value})}
-        placeholder='No. of players'
-        value={formData.players}
-      />
-
-      <input
-        type="text"
-        onChange={e => setFormData({...formData, 'publisher': e.target.value})}
-        placeholder='Publisher'
-        value={formData.publisher}
-      />
-
-      <input
-        type="file"
-        onChange={onChange}
-      />
-      <button onClick={createNote}>Create Note</button>
-
-
-
-      <div class="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 md:content-start md:justify-start mx-auto">
-      {
-          notes.map(note => (
-            <div key={note.id || note.name} className="w-64 rounded mx-auto">
-
-              {
-                note.image && <img src={note.image} className='w-64 h-64 object-cover rounded' />
-              }
-              <h2 className='py-3'>{note.name}</h2>
-              {/* <p>{note.description}</p>
-              <p>{note.genre}</p>
-              <p>{note.releaseDate}</p>
-              <p>{note.players}</p>
-              <p>{note.publisher}</p> */}
-              
-              {/* <button onClick={() => deleteNote(note)}>Delete note</button> */}
-            </div>
-          ))
-        }
+                {
+                  note.image && <img src={note.image} className='w-screen md:w-64 h-64 object-cover rounded' />
+                }
+                <h2 className='py-3 text-white'>{note.name}</h2>
+                {/* <p>{note.description}</p>
+                <p>{note.genre}</p>
+                <p>{note.releaseDate}</p>
+                <p>{note.players}</p>
+                <p>{note.publisher}</p> */}
+                
+                {/* <button onClick={() => deleteNote(note)}>Delete note</button> */}
+              </div>
+            ))
+          }
+          </div>
+          
+        </div>
       </div>
-      <div style={{marginBottom: 30}}>
+      <div className='container mx-auto py-12'>
+          <p className='text-center'><FontAwesomeIcon className='text-blue-500' icon="plus-square"/> Add a game to your library using the form below</p>
+        </div>
+      <div className='flex justify-center items-center mx-auto py-8'>
         
+        <div class="md:grid md:grid-cols-12 md:gap-6 w-2/4 drop-shadow-lg">
+          <div class="mt-5 md:mt-0 md:col-span-12">
+            <form>
+              <div class="shadow sm:rounded-md sm:overflow-hidden">
+                <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+                  <div class="grid grid-cols-12 gap-6">
+
+                          {/* Game Title */}
+
+                          <div class="col-span-6">
+                            <label for="title-name" class="block text-sm text-left font-medium text-gray-700 py-4">
+                              Game / Title name
+                            </label>
+                            <div class="mt-1 flex rounded-md shadow-sm">
+                              <input 
+                                type="text"
+                                onChange={e => setFormData({ ...formData, 'name': e.target.value })}
+                                placeholder='Title Name'
+                                value={formData.name}
+                                name="title-name"
+                                id="title-name"
+                                className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300 py-2 px-2"
+                              />
+                            </div>
+                          </div>
+
+                          {/* System Platform */}
+
+                          <div class="col-span-6">
+                          <label for="title-name" class="block text-sm text-left font-medium text-gray-700 py-4">
+                              System / Platform
+                            </label>
+                            <div class="mt-1 flex rounded-md shadow-sm">
+                              <input 
+                                type="text"
+                                onChange={e => setFormData({...formData, 'description': e.target.value})}
+                                placeholder='System Platform'
+                                value={formData.description}
+                                name="system-platform"
+                                id="system-platform"
+                                className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300 py-2 px-2"
+                              />
+                            </div>
+                          </div>
+                          
+                          {/* Title Genre */}
+
+                          <div class="col-span-6">
+                          <label for="title-name" class="block text-sm text-left font-medium text-gray-700 py-4">
+                              Title Genre
+                            </label>
+                            <div class="mt-1 flex rounded-md shadow-sm">
+                              <input 
+                                type="text"
+                                onChange={e => setFormData({...formData, 'genre': e.target.value})}
+                                placeholder='Title Genre'
+                                value={formData.genre}
+                                name="title-genre"
+                                id="title-genre"
+                                className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300 py-2 px-2"
+                              />
+                            </div>
+                          </div>
+                                                    
+                          {/* Release Date */}
+
+                          <div class="col-span-6">
+                          <label for="title-name" class="block text-sm text-left font-medium text-gray-700 py-4">
+                              Release Date
+                            </label>
+                            <div class="mt-1 flex rounded-md shadow-sm">
+                              <input 
+                                type="date" 
+                                onChange={e => setFormData({...formData, 'releaseDate': e.target.value})}
+                                placeholder='Release Date'
+                                value={formData.releaseDate}
+                                name="release-date"
+                                id="release-date"
+                                className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300 py-2 px-2 cursor-pointer"
+                              />
+                            </div>
+                          </div>
+                                                    
+                          {/* Number of Players */}
+
+                          <div class="col-span-6">
+                          <label for="title-name" class="block text-sm text-left font-medium text-gray-700 py-4">
+                              Number of Players
+                            </label>
+                            <div class="mt-1 flex rounded-md shadow-sm">
+                            <input 
+                                type="number"
+                                onChange={e => setFormData({...formData, 'players': e.target.value})}
+                                placeholder='No. of players'
+                                value={formData.players}
+                                name="player-count"
+                                id="player-count"
+                                className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300 py-2 px-2"
+                              />
+                            </div>
+                          </div>
+                                                                              
+                          {/* Publisher */}
+
+                          <div class="col-span-6">
+                          <label for="title-name" class="block text-sm text-left font-medium text-gray-700 py-4">
+                              Publisher
+                            </label>
+                            <div class="mt-1 flex rounded-md shadow-sm">
+                            <input 
+                                type="text"
+                                onChange={e => setFormData({...formData, 'publisher': e.target.value})}
+                                placeholder='Publisher'
+                                value={formData.publisher}
+                                name="publisher"
+                                id="publisher"
+                                className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300 py-2 px-2"
+                              />
+                            </div>
+                          </div>
+
+                          {/* Image upload */}
+
+                          <div className='col-span-12'>
+                          <label for="title-name" class="block text-sm text-left font-medium text-gray-700 py-4">
+                              Game / Title box art
+                            </label>
+                            <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                              <div class="space-y-1 text-left">
+                                <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                                  <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+                                <div class="flex justify-center text-sm text-gray-600">
+                                  <label for="file-upload" class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                                    <span>Upload a file</span>
+                                    <input
+                                    type="file"
+                                    onChange={onChange}
+                                    id="file-upload" 
+                                    name="file-upload" 
+                                    className="sr-only" />
+                                  </label>
+                                </div>
+                                <p class="text-xs text-gray-500">
+                                  PNG, JPG, GIF up to 10MB
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                        <button onClick={createNote} class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                          Create Game
+                        </button>
+                        
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div> 
       </div>
-      </div>
+      
     </main>
     </>
       )}
